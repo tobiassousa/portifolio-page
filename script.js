f46c66029e21b10c3a00da728a1491c7f6e1fd19
tobiassousa <tfbsousa@hotmail.com>
2025-02-11 22:15:37 -0300

    Primeira atualização

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const greetingElement = document.getElementById("greeting");
+
+const currentHour = new Date().getHours();
+
+
+
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia!"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde!"
+    : "Boa noite!";
+
+greetingElement.textContent = greetingMessage;
+
